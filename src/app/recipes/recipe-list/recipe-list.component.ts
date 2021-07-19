605663787812977400135aeb6a4551c8f9e46889
konepadith <74092214+konepadith@users.noreply.github.com>
2021-07-19 22:09:18 +0700

    ep 54 success

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[];
+  recipes:Recipe[]=[
+    new Recipe('A Test Recipe', 'This is simply a test','https://i8.amplience.net/i/traeger/Pittman-Tomahawk-Steak_Traeger-Wood-Fired-Grills_RE_HE_M?w=500&sm=aspect&aspect=5:3&scaleFit=poi&$poi2$&fmt=webp')
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -6,6 +6,6 @@ public imagePath:string;
   constructor(name:string, desc:string,imagePath:string){
     this.name=name;
     this.description=desc;
-    this.imagePath=imagePath
+    this.imagePath=imagePath;
   }
 }
